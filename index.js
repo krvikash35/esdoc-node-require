bdb34e29556863df38cbe25246e5d39b3082092b
Vikash Kumar <krvikash35@gmail.com>
2017-10-21 15:36:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 
 exports.onHandleCode = function onHandleCode(ev) {
   ev.data.code = ev.data.code
-    .replace(/(let|const|var)\s+([_\d\w]+)\s*=\s*require\('(.*)'\)/g,
-             "import $2 from '$3' ");
+    .replace(/(let|const|var)\s+([_\d\w]+)\s*=\s*require\('(.*)'\)(.*)/g, "import $2 from '$3' ");
 };
